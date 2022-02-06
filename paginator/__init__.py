953b08d90652ee36f30968f2bf5df240c7b772e1
Tommodev <tomm.pydev@gmail.com>
2022-02-06 21:48:06 +0100

    Added files

---
+++ paginator/__init__.py
@@ -0,0 +1,10 @@
+from .paginator import *
+from .objects import *
+
+__title__ = 'py-cord-paginator'
+__summary__ = 'Discord paginator for messages and embeds.'
+__uri__ = 'https://github.com/Tommodev-06/paginator.py'
+__author__ = 'Tommodev'
+__version__ = '0.0.1'
+__license__ = 'MIT'
+__copyright__ = '2022-present Tommodev.'
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pycord-paginator",
+    version="0.0.1",
+    author="Tommodev",
+    license="MIT",
+    description="Pycord paginator for messages and embeds with reactions and buttons.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Tommodev-06/paginator.py",
+    project_urls={
+        "Source": "https://github.com/Tommodev-06/paginator.py",
+        "Documentation": "https://flampt.gitbook.io/paginator"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    install_requires=['aiohttp', 'py-cord==2.0.0b3', 'asyncio'],
+    keywords='pycord paginator pycord-paginator',
+    packages=setuptools.find_packages(include=['paginator', 'paginator.*']),
+    python_requires=">=3.6",
+)
